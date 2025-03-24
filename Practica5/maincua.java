237a524556713678975ee45ec4e1adc9f82c694a
Cr7god12 <gabriel7elgoat@gmail.com>
2025-03-23 23:50:11 -0400

    Update maincua.java

--- Practica5/maincua.java
+++ Practica5/maincua.java
@@ -1,23 +1,28 @@
 import  java.util.Scanner;
 public class maincua {
-    
-    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Ingrese a: ");
+        System.out.print("Ingrese el valor de a: ");
         double a = scanner.nextDouble();
-        System.out.print("Ingrese b: ");
+        System.out.print("Ingrese el valor de b: ");
         double b = scanner.nextDouble();
-        System.out.print("Ingrese c: ");
+        System.out.print("Ingrese el valor de c: ");
         double c = scanner.nextDouble();
-        
 
-    
         EcuacionCuadratica ecuacion = new EcuacionCuadratica(a, b, c);
-        ecuacion.mostrarSolucion();
-        
+
+        double discriminante = ecuacion.getDiscriminante();
+
+        if (discriminante > 0) {
+            System.out.println("La ecuación tiene dos raíces: " + ecuacion.getRaiz1() + " y " + ecuacion.getRaiz2());
+        } else if (discriminante == 0) {
+            System.out.println("La ecuación tiene una raíz: " + ecuacion.getRaiz1());
+        } else {
+            System.out.println("La ecuación no tiene raíces reales.");
+        }
 
         scanner.close();
     }
 }
+    
