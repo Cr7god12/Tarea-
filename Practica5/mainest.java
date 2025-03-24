1f501f4e3e2932d39757f56e157adf7ab339b5ec
Cr7god12 <gabriel7elgoat@gmail.com>
2025-03-23 23:40:40 -0400

    Update mainest.java

--- Practica5/mainest.java
+++ Practica5/mainest.java
@@ -1,22 +1,22 @@
 import java.util.Scanner;
 
 public class mainest {
-   
-    	
-    	public static void main(String[] args) {
-    	Scanner scanner = new Scanner(System.in);
-    	double[] numeros = new double[10];
-
-    	System.out.println("Ingrese 10 números:");
-
-    	for (int i = 0; i < 10; i++) {
-    		numeros[i] = scanner.nextDouble();
-    	}
-
-    	Estadistica estadistica = new Estadistica(numeros);
-    	estadistica.mostrarResultados();
-
-    	scanner.close();
- 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double[] datos = new double[10];
+        
+        System.out.println("Ingrese 10 números :");
+        for (int i = 0; i < 10; i++) {
+            datos[i] = scanner.nextDouble();
+        }
+        
+        Estadistica estadistica = new Estadistica(datos);
+        double promedio = estadistica.Promedio();
+        double desviacion = estadistica.Desviacion();
+        
+        System.out.printf("El promedio es: %.2f\n", promedio);
+        System.out.printf("La desviación estándar es: %.2f\n", desviacion);
+        
+        scanner.close();
     }
 }
