8a3d39fc1a3c11ed261ca1f96a79638709af8331
Cr7god12 <gabriel7elgoat@gmail.com>
2025-03-24 00:09:27 -0400

    Update maincua.java

--- Practica5/maincua.java
+++ Practica5/maincua.java
@@ -1,4 +1,5 @@
 import  java.util.Scanner;
+
 public class maincua {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -14,10 +15,10 @@ public class maincua {
 
         double discriminante = ecuacion.getDiscriminante();
 
-         if (discriminante > 0) {
-            System.out.printf("La ecuación tiene dos raíces:"+ ecuacion.getRaiz1(), ecuacion.getRaiz2());
+        if (discriminante > 0) {
+            System.out.printf("La ecuación tiene dos raíces: %.4f y %.4f\n", ecuacion.getRaiz1(), ecuacion.getRaiz2());
         } else if (discriminante == 0) {
-            System.out.printf("La ecuación tiene una raíz:"+ ecuacion.getRaiz1());
+            System.out.printf("La ecuación tiene una raíz: %.4f\n", ecuacion.getRaiz1());
         } else {
             System.out.println("La ecuación no tiene raíces reales.");
         }
@@ -25,4 +26,3 @@ public class maincua {
         scanner.close();
     }
 }
-    
